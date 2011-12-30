5541384ed188c67edc77784d98391e93eb103bbd
Alan Orth <alan.orth@gmail.com>
2011-12-30 13:34:20 +0300

    u8150: Rename wireless interface from eth0 to wlan0
    Cosmetic fix.
    
    Change-Id: I637ffc30894b61b83e26e736d6fdbe9f97307bd2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,7 @@ TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 WIFI_DRIVER_FW_STA_PATH := "/system/etc/firmware/fw_bcm4319.bin"
 WIFI_DRIVER_FW_AP_PATH := "/system/etc/firmware/fw_bcm4319_apsta.bin"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/fw_bcm4319.bin nvram_path=/system/etc/firmware/nvram.txt"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/fw_bcm4319.bin nvram_path=/system/etc/firmware/nvram.txt iface_name=wlan"
 WIFI_DRIVER_MODULE_NAME := "dhd"
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4319.ko"
 WPA_SUPPLICANT_VERSION := VER_0_5_X
